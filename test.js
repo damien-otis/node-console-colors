abd3b08b4c7d3de67fad8b179ebd40adaf5cc761
Damien <damien@ylopo.com>
2020-10-19 11:31:00 -0700

    add links to github, add listcolors to test.js

--- test.js
+++ test.js
@@ -5,3 +5,5 @@ console.log(cc.set('fg_yellow','bg_blue', 'Hello', 'World'));
 console.log(cc.set('fg_red', 'Hello', 'World'));
 
 console.log(cc.set('bg_green', 'fg_purple', 'Hello', 'World'));
+
+cc.list();
